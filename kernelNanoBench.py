88f3972fad14e274c3cdcef52d2dd703513e6491
dimanteo <dikolobay@gmail.com>
2020-09-07 14:00:35 +0300

    Fixed the need for specifying the full path to android toolchain in script source code. Now only need to update system PATH variable.

--- kernelNanoBench.py
+++ kernelNanoBench.py
@@ -8,8 +8,6 @@ USING_ADB = False
 REMOTEDIR = '/data/local/tmp/'
 RAMDISK   = '/tmp/ramdisk/'
 AARCH64_BIN_PATH = '../../user/libs/arm64-v8a/'
-TOOLCHAIN = '/home/dimanteo/Home/AndroidDev/Sdk/ndk/21.0.6113669/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/'
-ADB_PATH = '/home/dimanteo/Home/AndroidDev/Sdk/platform-tools/'
 
 PFC_START_ASM = '.quad 0xE0b513b1C2813F04'
 PFC_STOP_ASM = '.quad 0xF0b513b1C2813F04'
@@ -28,7 +26,7 @@ def assemble(code, objFile, asmFile='/tmp/ramdisk/asm.s'):
          code = '.arch armv8-a\n' + code + ';1:;\n'
          with open(asmFile, 'w') as f: f.write(code);
          # should add $NDK_DIR/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin to PATH variable
-         subprocess.check_call([TOOLCHAIN + 'aarch64-linux-android-as', asmFile, '-o', objFile])
+         subprocess.check_call(['aarch64-linux-android-as', asmFile, '-o', objFile])
    except subprocess.CalledProcessError as e:
       sys.stderr.write('Error (assemble): ' + str(e) + '\n')
       sys.stderr.write(code)
@@ -40,7 +38,7 @@ def objcopy(sourceFile, targetFile):
       if not USING_ADB:
          subprocess.check_call(['objcopy', sourceFile, '-O', 'binary', targetFile])
       else:
-         subprocess.check_call([TOOLCHAIN + 'aarch64-linux-android-objcopy', sourceFile, '-O', 'binary', targetFile])
+         subprocess.check_call(['aarch64-linux-android-objcopy', sourceFile, '-O', 'binary', targetFile])
    except subprocess.CalledProcessError as e:
       sys.stderr.write('Error (objcopy): ' + str(e) + '\n')
       exit(1)
@@ -68,7 +66,7 @@ def getR14Size(using_adb):
 
 def adbPush(local, remote):
    try:
-      subprocess.call([ADB_PATH + 'adb', 'push', local, remote])
+      subprocess.call(['adb', 'push', local, remote])
    except subprocess.CalledProcessError as e:
       sys.stderr.write(str(e.returncode) + ' ADB could not push ' + local + ' to ' + remote + ' ' + '\n')
       raise e
@@ -76,7 +74,7 @@ def adbPush(local, remote):
 
 def adbPull(remote, local):
    try:
-      subprocess.call([ADB_PATH + 'adb', 'pull', remote, local])
+      subprocess.call(['adb', 'pull', remote, local])
    except subprocess.CalledProcessError as e:
       sys.stderr.write(str(e.returncode) + ' ADB could not pull ' + remote + ' to ' + local + '\n')
       raise e
@@ -84,7 +82,7 @@ def adbPull(remote, local):
 
 def adbExec(cmd):
    try:
-      output = subprocess.check_output([ADB_PATH + 'adb', 'shell'] + cmd)
+      output = subprocess.check_output(['adb', 'shell'] + cmd)
       return output
    except subprocess.CalledProcessError as e:
       query = ''
@@ -318,7 +316,7 @@ def createRamdisk():
 def deleteRamdisk():
    if ramdiskCreated:
       try:
-         subprocess.check_output('umount -l /tmp/ramdisk/', shell=True)
+         subprocess.check_output('sudo umount -l /tmp/ramdisk/', shell=True)
       except subprocess.CalledProcessError as e:
          sys.stderr.write('Could not delete ramdisk ' + e.output + '\n')
 
